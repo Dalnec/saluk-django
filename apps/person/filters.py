bc6eee71d2841fe99a9b06cd85082fba6a7f656f
Dalnec <dalnec1405@gmail.com>
2025-07-28 13:29:20 -0400

    fix: filter by search field

--- apps/person/filters.py
+++ apps/person/filters.py
@@ -4,18 +4,23 @@ from rest_framework.pagination import PageNumberPagination
 from .models import Patient
 
 class PatientFilter(django_filters.FilterSet):
-    patient_names = django_filters.CharFilter(method="filter_patient_names")
+    search = django_filters.CharFilter(method="filter_search")
 
     class Meta:
         model = Patient
         fields = [ "code", "ci", "names", "lastnames", "birthdate", "gender", 
-                  "phone", "email", "patient_names", ]
+                  "phone", "email", "search", ]
 
-    def filter_patient_names(self, queryset, name, value):
-        return queryset.filter(Q(names__icontains=value) | Q(lastnames__icontains=value))
+    def filter_search(self, queryset, name, value):
+        return queryset.filter(
+            Q(names__icontains=value) | 
+            Q(lastnames__icontains=value) | 
+            Q(ci__icontains=value) | 
+            Q(code__icontains=value)
+        )
 
 
 class PatientPagination(PageNumberPagination):
     page_size_query_param = "page_size"
     # max_page_size = 100
-    page_size = 20
+    page_size = 100
